8777b746826f0c61f4a442f7f2990e079d27ab54
Rafael Wogel <rafael.wogel96@gmail.com>
2019-08-11 14:19:36 -0300

    My Fist Commit

---
+++ src/pages/Login.js
@@ -0,0 +1,41 @@
+import React, { useState } from'react'
+import './Login.css'
+
+import api from '../services/api'
+
+import logo from '../assets/logo.svg'
+
+export default function Login({ history }) {
+
+    const [username, setUsername] = useState('');
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        console.log(username)
+
+        const response = await api.post('/devs', {
+            userName : username,    
+        });
+
+        const { _id } = response.data;
+
+        history.push(`/dev/${_id}`);
+    };
+
+    return(
+        <div className="login-container">
+            <form onSubmit={handleSubmit}>
+                <img src={logo} alt='Tindev'></img>
+                <input
+                placeholder="Digite seu Usuário no GitHub"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                />
+                <button 
+                type="submit">
+                    Enviar
+                </button>
+            </form>
+        </div>
+    );
+}
