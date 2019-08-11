34ecc977a6fe05861357eecd8b2dd570f2a79dfb
Rafael Wogel <rafael.wogel96@gmail.com>
2019-08-11 14:28:22 -0300

    Adicionado .env e process

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL : 'https://app-backend-tindev.herokuapp.com/api'
+    baseURL : process.env.REACT_APP_API_URL
 })
 
 export default api;
